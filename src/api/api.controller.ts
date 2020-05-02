a15833f9522e95c7f12c334f8fe12dac5c82a616
james@crenexi.com <james@crenexi.com>
2020-05-02 13:42:58 -0700

    WIP setup

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -2,8 +2,9 @@ import { Request, Response } from 'express';
 import getSemVer from '@helpers/get-sem-ver';
 
 const indexController = (_: Request, res: Response) => {
-  console.log(getSemVer());
-  res.render('pages/index.ejs');
+  const semVer = getSemVer();
+
+  res.render('pages/index.ejs', { semVer });
 };
 
 export default indexController;
--- src/helpers/get-sem-ver.ts
+++ src/helpers/get-sem-ver.ts
@@ -1,11 +1,16 @@
 import fs from 'fs';
 import path from 'path';
 
+interface PJson {
+  version?: string;
+}
+
 const getSemVer = () => {
   const root = path.resolve(__dirname, '..', '..');
   const contents = String(fs.readFileSync(`${root}/package.json`));
+  const pjson: PJson = contents ? JSON.parse(contents) : {};
 
-  return contents ? JSON.parse(contents) : {};
+  return pjson.version || '0.0.0';
 };
 
 export default getSemVer;
