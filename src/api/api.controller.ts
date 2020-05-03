3a9c605a6845bb4dddf6c6bcbc11abcbbcf57402
James Walrath <james@crenexi.com>
2020-05-02 17:48:05 -0700

    Ignore.

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -2,8 +2,8 @@ import { Request, Response } from 'express';
 import getSemVer from '@helpers/get-sem-ver';
 
 const indexController = (_: Request, res: Response) => {
-  const semVer = getSemVer();zasdf
-x
+  const semVer = getSemVer();
+
   res.render('pages/index.ejs', { semVer });
 };
 
